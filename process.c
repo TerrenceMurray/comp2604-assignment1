59dea5eeed7fc673789466b259883ca4a6451e57
Terrence Murray <terrencemurray004@gmail.com>
2025-02-12 11:58:41 -0400

    Remove comments from process.c

--- process.c
+++ process.c
@@ -1,68 +1,3 @@
-// #include <stdio.h>
-// #include <fcntl.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-
-// void create_processes(int depth, int fds[]) {
-//     // base case
-//     if (depth == 0)
-//         return;
-
-//     pid_t pid = fork();
-
-//     if (pid == -1) {
-//         perror("fork() failed. \n");
-//         exit(1);
-//     }
-
-//     if (pid == 0) {
-//         // child process
-//         close(fds[0]); // close the read end of pipe
-
-//         int id = getpid();
-//         write(fds[1], &id, sizeof(id));
-//     }
-    
-//     create_processes(depth - 1, fds);
-// }
-
-// int main () {
-//     int fds[2];
-
-//     // create pipe
-//     if (pipe(fds) == -1) {
-//         perror("pipe() failed. \n");
-//         exit(1);
-//     }
-
-//     // create processes
-//     create_processes(9, fds);
-
-//     close(fds[1]); // close the write end of pipe
-
-//     int even = 0, odd = 0, multiple10 = 0;
-
-//     // parent process
-//     int buffer;
-//     while (read(fds[0], &buffer, sizeof(buffer)) > 0) {
-//         // printf("Child PID: %d \n", buffer);
-
-//         if (buffer % 2 == 0)
-//             even++;
-//         else 
-//             odd++;
-
-//         if (buffer % 10 == 0)
-//             multiple10++;
-//     }
-
-//     close(fds[0]); // close read end of pipe when done
-
-//     printf("Even PIDs: %d\nOdd PIDs: %d\nMultiples of 10 PIDs: %d\n", even, odd, multiple10);
-
-//     return 0;
-// }
-
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -128,7 +63,7 @@ int main () {
     }
 
     close(fds[0]); // close read end of pipe when done
-    
+
     if (odd > 0 && even > 0 && multiple10 > 0)
         printf("Even PIDs: %d\nOdd PIDs: %d\nMultiples of 10 PIDs: %d\n", even, odd, multiple10);
 
